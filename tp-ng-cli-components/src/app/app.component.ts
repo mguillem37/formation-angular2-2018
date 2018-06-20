86a4f2174b49fcadc11db4ff214c0ae13acaab2d
Guillemette Matthieu <mguillem37@yahoo.fr>
2018-06-20 15:04:55 +0200

    add add-on

--- tp-ng-cli-components/src/app/app.component.ts
+++ tp-ng-cli-components/src/app/app.component.ts
@@ -7,8 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Bonjour Matthieu';
+  private username: string = 'Matthieu'
+  private users: object[] =[]
+  // private users:new Array<User>()
 
+  
   public onClick = (event:MouseEvent, name:string) => {
     console.log("onClick ! " + name +  " / " + event.clientX + " / " + event.clientY )
   }
+
+  public addUser = (lastname:string, firstname:string) => {
+    this.users.push({"firstname": firstname, "lastname": lastname})
+    console.log(this.users)
+  } 
 }
